ffe3a1ff2fcfe96efd61b2e5619627b6d18f39c3
Christopher Escalon <escalonchristopher@gmail.com>
2020-02-19 18:13:34 -0600

    removing comments

--- src/patterns/builder/roomBuilder.ts
+++ src/patterns/builder/roomBuilder.ts
@@ -13,45 +13,45 @@ interface RoomBuilder {
   Build(): Room;
 }
 
-// class BeachesRoomBuilder implements RoomBuilder {
-//   private room: Room = {
-//     name: "",
-//     price: 1,
-//     categories: [],
-//     attributes: []
-//   };
+class BeachesRoomBuilder implements RoomBuilder {
+  private room: Room = {
+    name: "",
+    price: 1,
+    categories: [],
+    attributes: []
+  };
 
-//   WithName(name: string): RoomBuilder {
-//     this.room.name = name;
-//     return this;
-//   }
+  WithName(name: string): RoomBuilder {
+    this.room.name = name;
+    return this;
+  }
 
-//   WithPrice(price: number): RoomBuilder {
-//     this.room.price = price;
-//     return this;
-//   }
+  WithPrice(price: number): RoomBuilder {
+    this.room.price = price;
+    return this;
+  }
 
-//   WithCategories(...categories: string[]): RoomBuilder {
-//     this.room.categories = categories;
-//     return this;
-//   }
+  WithCategories(...categories: string[]): RoomBuilder {
+    this.room.categories = categories;
+    return this;
+  }
 
-//   AppendAttribute(attribute: string): RoomBuilder {
-//     this.room.attributes.push(attribute);
-//     return this;
-//   }
+  AppendAttribute(attribute: string): RoomBuilder {
+    this.room.attributes.push(attribute);
+    return this;
+  }
 
-//   Build(): Room {
-//     return this.room;
-//   }
-// }
+  Build(): Room {
+    return this.room;
+  }
+}
 
-// const builder = new BeachesRoomBuilder();
+const builder = new BeachesRoomBuilder();
 
-// const room = builder
-//   .WithName("")
-//   .WithPrice(2000)
-//   .WithCategories("Hi", "Hi2")
-//   .AppendAttribute("Att1")
-//   .AppendAttribute("Att2")
-//   .Build();
+const room = builder
+  .WithName("")
+  .WithPrice(2000)
+  .WithCategories("Hi", "Hi2")
+  .AppendAttribute("Att1")
+  .AppendAttribute("Att2")
+  .Build();
--- src/patterns/composite/compositeVacation.ts
+++ src/patterns/composite/compositeVacation.ts
@@ -8,65 +8,65 @@ interface VacationComponent {
   add(component: VacationComponent): void;
 }
 
-// class ProductComposite implements VacationComponent {
-//   private readonly vacations: VacationComponent[];
-//   private readonly defaultPrice: number;
+class ProductComposite implements VacationComponent {
+  private readonly vacations: VacationComponent[];
+  private readonly defaultPrice: number;
 
-//   readonly name: string;
-//   readonly isComposite: boolean;
-//   readonly quantity: number;
-//   readonly discount: number;
-//   constructor(
-//     name: string,
-//     price?: number,
-//     quantity?: number,
-//     discount?: number
-//   ) {
-//     this.name = name || "";
-//     this.discount = discount ?? 0;
-//     this.isComposite = !price;
-//     this.defaultPrice = price ?? 500;
-//     this.quantity = quantity ?? 1;
-//     this.discount = discount ?? 0;
-//     this.vacations = [];
-//   }
+  readonly name: string;
+  readonly isComposite: boolean;
+  readonly quantity: number;
+  readonly discount: number;
+  constructor(
+    name: string,
+    price?: number,
+    quantity?: number,
+    discount?: number
+  ) {
+    this.name = name || "";
+    this.discount = discount ?? 0;
+    this.isComposite = !price;
+    this.defaultPrice = price ?? 500;
+    this.quantity = quantity ?? 1;
+    this.discount = discount ?? 0;
+    this.vacations = [];
+  }
 
-//   add(vacation: VacationComponent): void {
-//     this.vacations.push(vacation);
-//   }
+  add(vacation: VacationComponent): void {
+    this.vacations.push(vacation);
+  }
 
-//   get price() {
-//     return this.isComposite
-//       ? this.vacations.reduce(
-//           (previous, current) => previous + current.price,
-//           0
-//         )
-//       : this.defaultPrice;
-//   }
+  get price() {
+    return this.isComposite
+      ? this.vacations.reduce(
+          (previous, current) => previous + current.price,
+          0
+        )
+      : this.defaultPrice;
+  }
 
-//   get subTotal() {
-//     if (this.isComposite) {
-//       return this.price * this.quantity;
-//     }
+  get subTotal() {
+    if (this.isComposite) {
+      return this.price * this.quantity;
+    }
 
-//     return (
-//       this.vacations.reduce(
-//         (previous, current) => previous + current.subTotal,
-//         0
-//       ) *
-//         this.quantity -
-//       this.discount
-//     );
-//   }
-// }
+    return (
+      this.vacations.reduce(
+        (previous, current) => previous + current.subTotal,
+        0
+      ) *
+        this.quantity -
+      this.discount
+    );
+  }
+}
 
-// const vaction1 = new ProductComposite("Room 1", 900);
-// const vaction2 = new ProductComposite("Room 2", 900);
-// const vaction3 = new ProductComposite("Room 3", 900);
+const vaction1 = new ProductComposite("Room 1", 900);
+const vaction2 = new ProductComposite("Room 2", 900);
+const vaction3 = new ProductComposite("Room 3", 900);
 
-// const promo = new ProductComposite("Promo");
-// promo.add(vaction1);
-// promo.add(vaction2);
-// promo.add(vaction3);
+const promo = new ProductComposite("Promo");
+promo.add(vaction1);
+promo.add(vaction2);
+promo.add(vaction3);
 
-// console.log(promo.subTotal);
+console.log(promo.subTotal);
